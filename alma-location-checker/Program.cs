5c8f83cb4dfd156fed1fd8005f9d26d7e9d5d65f
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2021-01-24 12:44:42 +0100

    Base

---
+++ alma-location-checker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alma_location_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
