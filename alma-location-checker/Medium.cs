ea244079730d0f29b66378e0f8569d6730ef3354
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2021-02-03 12:42:36 +0100

    adapt regex to mach LN barcodes

--- alma-location-checker/Medium.cs
+++ alma-location-checker/Medium.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 namespace alma_location_checker {
     public class Medium {
         internal bool CheckBarcode(string barcode) {
-            Regex newBarcodePattern = new Regex(@"\+XAW\d+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Regex newBarcodePattern = new Regex(@"\+XAW\d+\w?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
             Regex oldBarcodePattern = new Regex(@"\d{8}", RegexOptions.Compiled); // We don't need to be case insensitive here
             
             if (oldBarcodePattern.IsMatch(barcode) || newBarcodePattern.IsMatch(barcode)) {
