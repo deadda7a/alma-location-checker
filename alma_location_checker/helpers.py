f849d730786ca845dc3015e3f4fd875e91592da1
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2021-02-10 10:39:59 +0100

    code style

--- alma_location_checker/helpers.py
+++ alma_location_checker/helpers.py
@@ -17,18 +17,18 @@ def mediumData(mediumDataFromApi, term):
     }
 
     if mediumDataFromApi["item_data"]["location"]["value"] == "MAG4":  # Location of the item, L, LN, MAG1-4
-        medium["location"] = "{t.blue}{0}{t.normal}".format(mediumDataFromApi["item_data"]["location"]["value"], t=term)  # MAG4 is blue
+        medium["location"] = term.on_blue(mediumDataFromApi["item_data"]["location"]["value"])  # MAG4 is blue
     elif mediumDataFromApi["item_data"]["location"]["value"] == "MAG3":
-        medium["location"] = "{t.yellow}{0}{t.normal}".format(mediumDataFromApi["item_data"]["location"]["value"], t=term)  # MAG3 is yellow
+        medium["location"] = term.on_yellow(mediumDataFromApi["item_data"]["location"]["value"])  # MAG3 is yellow
     else:
         medium["location"] = mediumDataFromApi["item_data"]["location"]["value"]  # All other locations don't have a color
 
     if not mediumDataFromApi["item_data"]["base_status"]["value"]: # Is the item available? 
         if mediumDataFromApi["item_data"]["process_type"]["desc"] == "Loan":
-            medium["workOrderType"] = "{t.red}Loan{t.normal}".format(t=term)
+            medium["workOrderType"] = term.bright_red("Loan")
         else:
             # something like "Acquisition technical services"
-            medium["workOrderType"] = "{t.red}{0}{t.normal}".format(mediumDataFromApi["item_data"]["work_order_type"]["desc"], t=term)
+            medium["workOrderType"] = term.bright_red(mediumDataFromApi["item_data"]["work_order_type"]["desc"])
     else:
         medium["workOrderType"] = ""
 
